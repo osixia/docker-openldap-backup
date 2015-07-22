61df1416ca771901a3042ea1e76fdf784a95c043
ofreax <bertrand.gouny@gmail.com>
2015-07-22 21:53:19 +0200

    modify baseimage tool

--- image/service/slapd-backup/container-start.sh
+++ image/service/slapd-backup/container-start.sh
@@ -5,9 +5,6 @@ FIRST_START_DONE="/etc/docker-openldap-backup-first-start-done"
 # container first start
 if [ ! -e "$FIRST_START_DONE" ]; then
 
-  # Install tools
-  ln -s /osixia/service/slapd-backup/assets/tool/* /sbin/
-
   # Adapt cronjobs file
   sed -i "s|{{ CRON_EXP_BACKUP_CONFIG }}|${CRON_EXP_BACKUP_CONFIG}|g" /osixia/service/slapd-backup/assets/cronjobs
   sed -i "s|{{ CRON_EXP_BACKUP_DATA }}|${CRON_EXP_BACKUP_DATA}|g" /osixia/service/slapd-backup/assets/cronjobs
---
+++ image/service/slapd-backup/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+# this script is run during the image build
+
+# Install image tools
+ln -s /osixia/service/slapd-backup/assets/tool/* /sbin/
