9bcab4c91391d6726b349a0275ba5b3e793057c1
ofreax <bertrand.gouny@gmail.com>
2015-08-03 19:56:40 +0200

    -

--- image/service/slapd-backup/container-start.sh
+++ image/service/slapd-backup/container-start.sh
@@ -5,7 +5,7 @@ FIRST_START_DONE="/etc/docker-openldap-backup-first-start-done"
 # container first start
 if [ ! -e "$FIRST_START_DONE" ]; then
 
-  # Adapt cronjobs file
+  # adapt cronjobs file
   sed -i "s|{{ LDAP_BACKUP_CONFIG_CRON_EXP }}|${LDAP_BACKUP_CONFIG_CRON_EXP}|g" /container/service/slapd-backup/assets/cronjobs
   sed -i "s|{{ LDAP_BACKUP_DATA_CRON_EXP }}|${LDAP_BACKUP_DATA_CRON_EXP}|g" /container/service/slapd-backup/assets/cronjobs
 
--- image/service/slapd-backup/install.sh
+++ image/service/slapd-backup/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 # this script is run during the image build
 
-# Install image tools
+# install image tools
 ln -s /container/service/slapd-backup/assets/tool/* /sbin/
 
-# Add cron jobs
+# add cron jobs
 ln -s /container/service/slapd-backup/assets/cronjobs /etc/cron.d/slapd-backup
 chmod 600 /container/service/slapd-backup/assets/cronjobs
