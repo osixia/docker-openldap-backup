4c68cdeea904bd8c53292a53a3b8f9143fe1b392
Bertrand Gouny <bgouny@hipay.com>
2016-01-18 13:02:49 +0100

    make it friendly with --copy-service

--- image/service/slapd-backup/startup.sh
+++ image/service/slapd-backup/startup.sh
@@ -4,18 +4,16 @@
 # https://github.com/osixia/docker-light-baseimage/blob/stable/image/tool/log-helper
 log-helper level eq trace && set -x
 
+# add image tools to path
+ln -sf ${CONTAINER_SERVICE_DIR}/slapd-backup/assets/tool/* /sbin/
+
+# add cron jobs
+ln -sf ${CONTAINER_SERVICE_DIR}/slapd-backup/assets/cronjobs /etc/cron.d/slapd-backup
+chmod 600 ${CONTAINER_SERVICE_DIR}/slapd-backup/assets/cronjobs
 
 FIRST_START_DONE="${CONTAINER_STATE_DIR}/docker-openldap-backup-first-start-done"
-
 # container first start
 if [ ! -e "$FIRST_START_DONE" ]; then
-
-  # install image tools
-  ln -s ${CONTAINER_SERVICE_DIR}/slapd-backup/assets/tool/* /sbin/
-
-  # add cron jobs
-  ln -s ${CONTAINER_SERVICE_DIR}/slapd-backup/assets/cronjobs /etc/cron.d/slapd-backup
-  chmod 600 ${CONTAINER_SERVICE_DIR}/slapd-backup/assets/cronjobs
 
   # adapt cronjobs file
   sed -i "s|{{ LDAP_BACKUP_CONFIG_CRON_EXP }}|${LDAP_BACKUP_CONFIG_CRON_EXP}|g" ${CONTAINER_SERVICE_DIR}/slapd-backup/assets/cronjobs
